5d2a419456c40e917efa63088bd868a505215672
Ivan Sivak <ivansivak26@gmail.com>
2016-10-15 08:41:54 +0300

    HW #3

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from finance.views import hello_world
+from finance.views import charges
 
 urlpatterns = [
+    url(r'charges', charges),
     url(r'.*', hello_world),
 ]
