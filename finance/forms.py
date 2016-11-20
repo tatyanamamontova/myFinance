3b2746b551b49764127d9f0185b1da52861e9d3b
Tatyana <taya_violet@yandex.ru>
2016-11-20 18:53:34 +0300

    Test commit

--- finance/forms.py
+++ finance/forms.py
@@ -1,22 +1,52 @@
-from django.forms import Form, DecimalField, DateField
-from django.utils import timezone
+# from django.forms import Form, DecimalField, DateField
+# from django.utils import timezone
+#
+#
+# class ChargeForm(Form):
+#
+#     value = DecimalField(
+#         label='Сумма',
+#         required=True
+#     )
+#
+#     date = DateField(
+#         label='Дата',
+#         required=True
+#     )
+#
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         now = timezone.now().date()
+#         if cleaned_data.get('value', 0) < 0 and cleaned_data.get('date') >= now:
+#             self.add_error('date', 'Нельзя заводить списание на будущее')
+#         return cleaned_data
+from datetime import date
+from django.forms import ModelForm, fields
+from finance.models import Charge, Account
+class ChargeForm(ModelForm):
 
-
-class ChargeForm(Form):
-
-    value = DecimalField(
-        label='Сумма',
-        required=True
-    )
-
-    date = DateField(
-        label='Дата',
-        required=True
-    )
+    class Meta:
+        model = Charge
+        fields = ['date','value']
 
     def clean(self):
-        cleaned_data = super().clean()
-        now = timezone.now().date()
-        if cleaned_data.get('value', 0) < 0 and cleaned_data.get('date') >= now:
-            self.add_error('date', 'Нельзя заводить списание на будущее')
+        cleaned_data = super(ChargeForm, self).clean()
+        date_charge = cleaned_data.get('date')
+        value_charge = cleaned_data.get('value')
+        today =  date.today()
+        if  value_charge < 0:
+            if date_charge > today:
+                self.add_error('date',"Impossible to make a write-off")
+                self.add_error('value',"Impossible to make a write-off")
         return cleaned_data
+
+    def save(self, user):
+        obj = super(ChargeForm, self).save(commit=False)
+        obj.account = user
+        return obj.save()
+
+class AccountForm(ModelForm):
+
+    class Meta:
+        model = Account
+        fields = ['account_id','income','outcome']
--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Charge(models.Model):  # денежная транзакция
+
+    date = models.DateField(label='date')
+    value = models.DecimalField(label='value')
+    account = models.ForeignKey(Account, related_name='charge')
+
+
+class Account(models.Model):  # банковский счет
+
+    account_id = models.CharField(max_length=300)
+    income = models.DecimalField(label='income')
+    outcome = models.DecimalField(label='outcome')
+    total = models.DecimalField(label='total')
+
+    class Meta:
+        db_table1 = 'income'
+        db_table2 = 'outcome'
