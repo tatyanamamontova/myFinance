fb6a6b9b94466f676cf7b5e9147d1662e8d66291
Ivan Sivak <ivan@sivak.tech>
2016-11-25 11:52:16 +0300

    Many changes

--- finance/models.py
+++ finance/models.py
@@ -1,21 +1,63 @@
 from django.db import models
+from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from datetime import datetime
+from django.db.models import Count, Sum, Avg
+from django.db.models.functions import TruncMonth
 
 
 # Bank account
 class Account(models.Model):
 
-    account_id = models.CharField(max_length=300)
-    income = models.DecimalField(max_digits=12, decimal_places=10)
-    outcome = models.DecimalField(max_digits=12, decimal_places=10)
-    total = models.DecimalField(max_digits=12, decimal_places=10)
+    account_holder = models.CharField(max_length=300)
+    income = models.DecimalField(max_digits=6, decimal_places=2)
+    outcome = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.account_holder)
 
     class Meta:
         db_table = 'charges'
+
+
+# Validate account creation
+# class CreateAccount(ModelForm):
+#
+#     class Meta:
+#         model = Account
+#         fields = ['account_id','income','outcome']
+#
+#     def clean_account_id(self):
+#         id = self.cleaned_data.get('account_id')
+#         if id != "One":
+#             raise ValidationError("Not valid")
+#         return id
 
 
 # Transaction
 class Charge(models.Model):
 
     date = models.DateField()
-    value = models.DecimalField(max_digits=12, decimal_places=10)
-    account = models.ForeignKey(Account, related_name='charge')
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return str(self.date) + " : " + str(self.value) + " : " + str(self.account)
+
+    @classmethod
+    def get_incomes(cls, account):
+        return cls.objects.filter(account=account).filter(value__gte=0)
+
+    @classmethod
+    def get_outcomes(cls, account):
+        return cls.objects.filter(account=account).filter(value__lt=0)
+
+    @classmethod
+    def get_by_month(cls, account):
+        return cls.objects\
+            .filter(account=account)\
+            .annotate(month=TruncMonth('date'))\
+            .values('month')\
+            .annotate(c=Avg('value'))\
+            .values('month', 'c')
--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
-from .forms import ChargeForm
+from .forms import ChargeForm, CreateAccount, CreateTransaction
+from .models import Account, Charge #CreateAccount
+from django.db.models import F
 
 # from models.RandomTransactions import random_transactions
 # from models.Charge import Charge
+
+from django.utils import timezone
+from datetime import datetime
 
 
 def index(request):
@@ -11,16 +17,99 @@ def index(request):
     return render(request, 'index.html', {'form': form})
 
 
-def charges(request):
-    pass
-    # transactions = random_transactions()
-    # positive = list()
-    # negative = list()
-    # for each in transactions:
-    #     if each[1] > 0:
-    #         positive.append(each)
-    #     else:
-    #         negative.append(each)
-    #
-    # return render(request, 'charges.html', {'positive': positive,
-    #                                         'negative': negative})
+def charges(request, account_holder):
+
+    account = Account.objects.get(account_holder=account_holder)
+
+    incomes = Charge.get_incomes(account)
+    outcomes = Charge.get_outcomes(account)
+
+    return render(request, 'charges.html', {'account': account,
+                                            'incomes': incomes,
+                                            'outcomes': outcomes})
+    return HttpResponse("Charges")
+
+
+def create_account(request):
+
+    if request.method == 'POST':
+        form = CreateAccount(request.POST)
+        if form.is_valid():
+            account = Account(account_holder=form.cleaned_data["account_holder"],
+                              income=0,
+                              outcome=0,
+                              total=0)
+            account.save()
+            return HttpResponse(form.cleaned_data["account_holder"])
+        else:
+            return HttpResponse("Not valid")
+
+    return render(request, 'create_account.html')
+
+
+def create_charge(request, account_holder):
+
+    account = Account.objects.get(account_holder=account_holder)
+
+    if request.method == 'POST':
+        form = CreateTransaction(request.POST)
+        if form.is_valid():
+            charge = Charge(account=account,
+                            value=form.cleaned_data['transaction'],
+                            date=timezone.datetime.now())
+            charge.save()
+            Account.objects.filter(account_holder=account_holder)\
+                .update(total=F('total') + form.cleaned_data['transaction'])
+            return HttpResponse("Done")
+
+        return HttpResponse("Error")
+
+    return render(request, 'charge.html')
+
+
+def get_all_accounts(request):
+    all_accounts = Account.objects.get_queryset()
+    print("All accounts: ")
+    print(all_accounts)
+    return render(request, 'all_accounts.html', {'all_accounts' : all_accounts})
+
+
+def account_view(request, account_holder):
+
+    account = Account.objects.get(account_holder=account_holder)
+
+    if request.method == 'POST':
+        form = CreateTransaction(request.POST)
+        if form.is_valid():
+            charge = Charge(account=account,
+                            value=form.cleaned_data['transaction'],
+                            # date=timezone.datetime.now())
+                            date = datetime(2015, 6, 10))
+            charge.save()
+            Account.objects.filter(account_holder=account_holder)\
+                .update(total=F('total') + form.cleaned_data['transaction'])
+            return HttpResponse("POST OK")
+        return HttpResponse("Method POST")
+
+    else:
+        try:
+            outcomes = Charge.get_outcomes(account)
+            incomes = Charge.get_incomes(account)
+            months = Charge.get_by_month(account)
+            print(months)
+        except ObjectDoesNotExist:
+            print("Can't find")
+        return render(request, 'account.html', {'account': account,
+                                                'incomes': incomes,
+                                                'outcomes': outcomes,
+                                                'months': months})
+
+
+def months(request, account_holder):
+
+    try:
+        account = Account.objects.get(account_holder=account_holder)
+        by_months = Charge.get_by_month(account)
+    except ObjectDoesNotExist:
+        print("Can't find")
+    return render(request, 'months.html', {'account': account, 'months': by_months})
